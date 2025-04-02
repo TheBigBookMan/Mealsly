ad1cca8919bc012106415dfd38d00f6295b3fd7a
Ben Smerd <bsmerd@adam.com.au>
2025-04-02 17:40:36 +1030

    setup navbar to be dynamic

--- client/src/components/common/Navbar/Navbar.tsx
+++ client/src/components/common/Navbar/Navbar.tsx
@@ -2,19 +2,10 @@ import { useUser } from "../../../contexts/UserContext";
 import { Link } from "react-router-dom";
 import { MapPinIcon, MagnifyingGlassIcon, ChatBubbleLeftIcon, UserCircleIcon, ListBulletIcon } from '@heroicons/react/24/outline';
 import { useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 const Navbar = () => {
     const {user, loading} = useUser();
-    const [selectedNav, setSelectedNav] = useState<string>('');
     const {pathname} = useLocation();
-    
-    useEffect(() => {
-        if(pathname) {
-            console.log(pathname);
-            
-        }
-    }, [pathname]);
 
     return (
         <ul className='flex justify-around items-center md:hidden h-[60px] border-t'>
@@ -33,15 +24,19 @@ const Navbar = () => {
                 <p className='text-xs'>Listings</p>
             </Link>
 
-            <Link to="/messages" className={`flex flex-col items-center ${pathname === '/messages' && 'text-sky-600 font-bold'}`}>
-                <ChatBubbleLeftIcon className="w-6 h-6 " />
-                <p className='text-xs'>Messages</p>
-            </Link>
+            {user && (
+                <Link to="/messages" className={`flex flex-col items-center ${pathname === '/messages' && 'text-sky-600 font-bold'}`}>
+                    <ChatBubbleLeftIcon className="w-6 h-6 " />
+                    <p className='text-xs'>Messages</p>
+                </Link>
+            )}
 
-            <Link to="/profile" className={`flex flex-col items-center ${pathname === '/profile' && 'text-sky-600 font-bold'}`}>
-                <UserCircleIcon className="w-6 h-6 " />
-                <p className='text-xs'>Profile</p>
-            </Link>
+            {user && (
+                <Link to="/profile" className={`flex flex-col items-center ${pathname === '/profile' && 'text-sky-600 font-bold'}`}>
+                    <UserCircleIcon className="w-6 h-6 " />
+                    <p className='text-xs'>Profile</p>
+                </Link>
+            )}
         </ul>
     )
 }
