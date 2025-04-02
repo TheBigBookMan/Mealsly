019ea4ae7430d65930eb0b53e9880fbbe10bdbff
Ben Smerd <bsmerd@adam.com.au>
2025-04-02 17:40:30 +1030

    create pages

---
+++ client/src/pages/NotificationsPage.tsx
@@ -0,0 +1,9 @@
+const NotificationsPage = () => {
+    return (
+        <div>
+            <p>Notifications</p>
+        </div>
+    )
+}
+
+export default NotificationsPage;
---
+++ client/src/pages/RegisterChefPage.tsx
@@ -0,0 +1,9 @@
+const RegisterChefPage = () => {
+    return (
+        <div>
+            <p>registe rchef</p>
+        </div>
+    )
+}
+
+export default RegisterChefPage;
