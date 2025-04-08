df7eab151cfb816d1133981ba8ac7658465cdfdf
Ben Smerd <bsmerd@adam.com.au>
2025-04-08 21:36:21 +0930

    feature(frontend): add in the Listing interface type

--- client/src/types/global.d.ts
+++ client/src/types/global.d.ts
@@ -4,4 +4,15 @@ declare global {
     interface User {
         username: string;
     }
+
+    interface Listing {
+        id: string;
+        title: string;
+        description: string;
+        price: number;
+        image: string;
+        available: boolean;
+        createdAt: string;
+
+    }
 };
