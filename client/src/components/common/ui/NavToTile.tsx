3773b1f1c737275776b31985d8e52e5d6f25694e
Ben Smerd <bsmerd@adam.com.au>
2025-04-12 14:10:11 +0930

    feature(frontend): create the NavToTile for navigation

---
+++ client/src/components/common/ui/NavToTile.tsx
@@ -0,0 +1,23 @@
+import { ChevronRightIcon } from "@heroicons/react/24/outline"
+import { Link } from "react-router-dom"
+
+interface NavToTileInterface {
+    navTo: string;
+    title: string;
+    subtitle: string | null;
+}
+
+const NavToTile = ({navTo, title, subtitle}: NavToTileInterface) => {
+    return (
+        <Link to={navTo} className='h-[60px] w-full flex justify-between items-center hover:cursor-pointer hover:bg-slate-200 rounded-xl gap-4 border-2 shadow py-2 px-4 transition'>
+            <div className='flex flex-col justify-center'>
+                <p className='text-sky-700 font-bold'>{title}</p>
+                <p className='text-sm'>{subtitle}</p>
+            </div>
+
+            <ChevronRightIcon className='w-6' />
+        </Link>
+    )
+}
+
+export default NavToTile
