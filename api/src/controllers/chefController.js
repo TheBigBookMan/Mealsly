b051b687762cd7b83e28080a4386c13f9639a159
Ben Smerd <bsmerd@adam.com.au>
2025-04-14 19:18:22 +0930

    refactor(api): fix up client calling for prisma

--- api/src/controllers/chefController.js
+++ api/src/controllers/chefController.js
@@ -1,4 +1,4 @@
-const { prisma } = require('../db');
+const { prisma } = require('../db/prisma');
 
 class ChefController {
     async createChef (req, res) {
--- api/src/controllers/eaterController.js
+++ api/src/controllers/eaterController.js
@@ -1,4 +1,4 @@
-const {prisma} = require('../db');
+const {prisma} = require('../db/prisma');
 
 class EaterController {
     async createEater (req, res) {
