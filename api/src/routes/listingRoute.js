88edaff3d0445b7e06a947504517c556399e2bd5
Benjamin Smerd <bjsmerd@gmail.com>
2025-05-10 11:48:52 +0930

    feature(db): add in the createListing route for listing

--- api/src/routes/listingRoute.js
+++ api/src/routes/listingRoute.js
@@ -1,5 +1,14 @@
-
+const express = require('express');
+const router = express.Router();
+const listingController = require('../controllers/listingController');
+const verifyFirebaseToken = require('../middleware/verifyFirebaseToken');
 
 
 // TODO add in verifyfirebase middleware
 // TODO need to break up api into controllers/model/service and add in the include flags where applicable
+
+// * /
+// Post create a listing
+router.post('/', listingController.createListing);
+
+module.exports = router;
