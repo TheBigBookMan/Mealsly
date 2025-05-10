94fa1d7e6efb8a985142ba29468e7c1d8fd10e3c
Benjamin Smerd <bjsmerd@gmail.com>
2025-05-10 11:59:06 +0930

    feature(api): create the route for getAllTags

---
+++ api/src/routes/tagRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const tagController = require('../controllers/tagController');
+const verifyFirebaseToken = require('../middleware/verifyFirebaseToken');
+
+// TODO add in verifyfirebase middleware
+// TODO need to break up api into controllers/model/service and add in the include flags where applicable
+
+// * /
+// Get all tags
+router.get('/', tagController.getAllTags);
+
+module.exports = router;
