bd325b2fa074ec3e3c0b97340eaa0bb78435b9ca
Benjamin Smerd <bjsmerd@gmail.com>
2025-05-10 12:35:45 +0930

    feature(api): create the createTag controller for tag

--- api/src/controllers/tagController.js
+++ api/src/controllers/tagController.js
@@ -11,6 +11,27 @@ class TagController {
             errorHttp(res, err, 'Error fetching tags:', 500);
         }
     }
+
+    async createTag (req, res) {
+        const {name, description} = req.body;
+        
+        try {
+
+            const tag = await prisma.tag.create({
+                data: {
+                    name,
+                    description: description ? description : ''
+                }
+            });
+
+            // TODO do the prisma check for insert duplicate code
+            
+            return res.status(201).json({success: true, tag});
+
+        } catch(err) {
+            errorHttp(res, err, 'Error creating tag', 500);
+        }
+    }
 }
 
 module.exports = new TagController();
