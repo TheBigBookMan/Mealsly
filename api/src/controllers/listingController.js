bbb15e96d82a195026364e79cb75402d94b79263
Benjamin Smerd <bjsmerd@gmail.com>
2025-05-10 20:20:36 +0930

    feature(api): create the createListing listing controller

--- api/src/controllers/listingController.js
+++ api/src/controllers/listingController.js
@@ -1,25 +1,43 @@
 const { prisma } = require('../db/prisma');
 const listingService = require('../services/listingService');
+const chefModel = require('../models/chefModel');
 const {errorHttp} = require('../utils/errors');
-
-/*
-title String
-    description String
-    price Float
-    image String?
-    available Boolean @default(true)
-    createdAt DateTime @default(now())
-
-    delivery Boolean @default(false)
-    pickup Boolean @default(true)
-    pickupAddressLatLon String?
-*/
+const cuisineModel = require('../models/cuisineModel');
 
 // TODO add in dietrytags as well when created but this will need to connect to tags created- through listingTag table
 
 class ListingController {
     async createListing (req, res) {
+        const {title, description, price, image, available, cuisineId, chefId, delivery, pickup, pickupAddressLatLon} = req.body;
 
+        try {
+
+            const chef = await chefModel.getExistingChefId(chefId);
+            if(!chef) return errorHttp(res, `ChefId: ${chefId}`, 'Chef does not exist', 400);
+
+            const cuisine = await cuisineModel.getExistingCuisineId(cuisineId);
+            if(!cuisine) return errorHttp(res, `CuisineId: ${cuisineId}`, 'Cuisine does not exist', 400);
+
+            // TODO need to add image to S3 bucket
+
+            await listingModel.create({
+                title,
+                description,
+                price,
+                cuisineId,
+                chefId,
+                image,
+                delivery,
+                pickup,
+                pickupAddressLatLon,
+                available
+            });
+
+            return res.status(201).json({ success: true, listing});
+            
+        } catch(err) {
+            errorHttp(res, err, 'Error creating listing', 500);
+        }
     }
 }
 
