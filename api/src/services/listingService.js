4304bf8b5bb59f8f89b0dcd029680e063b13dde0
Ben Smerd <bsmerd@adam.com.au>
2025-05-11 13:11:52 +0930

    feature(api): add the createListing service for Listing

--- api/src/services/listingService.js
+++ api/src/services/listingService.js
@@ -1,7 +1,39 @@
 const listingModel = require('../models/listingModel');
 
 class ListingService {
-    
+    async createListing (data, file) {
+        const {title, description, price, available, cuisineId, chefId, delivery, pickup, pickupAddressLatLon} = data;
+
+        // TODO better error handling for throwing errors to be caught in controller
+
+        // ? Validate chef
+        const chef = await chefModel.getExistingChefId(chefId);
+        if(!chef) throw new Error(`Chef with ID ${chefId} does not exist`);
+
+        // ? Validate cuisine
+        const cuisine = await cuisineModel.getExistingCuisineId(cuisineId);
+        if(!cuisine)  throw new Error(`Cuisine with ID ${cuisineId} does not exist`);
+
+            // TODO need to add image to S3 bucket- need S3 util
+
+        
+        // ? Create the listing
+        return await listingModel.createListing({
+            title,
+            description,
+            price,
+            cuisineId,
+            chefId,
+            image: imageUrl,
+            available,
+            delivery,
+            pickup,
+            pickupAddressLatLon,
+            dietryTags: tagIds ? {
+                create: tagIds.map(tagId => ({ tagId })),
+            } : undefined,
+        });
+    }
 }
 
 module.exports = new ListingService();
