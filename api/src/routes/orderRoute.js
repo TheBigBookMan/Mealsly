0c4ff83119097132ebb132e313fda7bcbd50bf08
Benjamin Smerd <bjsmerd@gmail.com>
2025-05-12 19:03:37 +0930

    feature(api): create the createOrder route

--- api/src/routes/orderRoute.js
+++ api/src/routes/orderRoute.js
@@ -1,5 +1,15 @@
-
-
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controllers/orderController');
+const verifyFirebaseToken = require('../middleware/verifyFirebaseToken');
 
 // TODO add in verifyfirebase middleware
 // TODO need to break up api into controllers/model/service and add in the include flags where applicable
+
+// * /
+// Post create a new order
+router.post('/', orderController.createOrder);
+
+// Get a single order
+
+// Get all orders from an account
